23d16526a629b1e7809507fb698cfd7aa1d01a6b
kelandisi <1191054822@qq.com>
2019-05-10 13:53:54 +0800

    更新代码

--- 作业代码/作业5/guess number/guess number/text_2.c
+++ 作业代码/作业5/guess number/guess number/text_2.c
@@ -1,60 +1,61 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-void menu()
-{
-	printf("******************************\n");
-	printf("***   1.play      0.exit  **\n");
-	printf("******************************\n");
-}
 void game()
-{	
-	int ret = rand()%100+1;
+{
+	int ret = 0;
+	ret = rand() % 100 + 1;
+	int x = 0;
 	while (1)
 	{
-		int a = 0;
-		printf("猜一个数:<");
-		scanf("%d", &a);
-		if (a < ret)
+		printf("请输入你要猜的数字<:");
+		scanf("%d", &x);
+		if (x>ret)
 		{
-			printf("猜小了\n");
+			printf("猜大了！\n");
 		}
-		else if (a>ret)
+		if (x<ret)
 		{
-			printf("猜大了\n");
+			printf("猜小了！\n");
 		}
-		else
+		if (ret == x)
 		{
-			printf("恭喜你猜对了！\n");
+			prinft("恭喜你猜对了！\n");
 			break;
 		}
 	}
 }
-int main()
+void menu()
 {
-	srand((unsigned int)time(NULL));
+	printf("******************\n");
+	printf("***   1.play   ***\n");
+	printf("***   0.exit   ***\n");
+	printf("******************\n");
+}
+void test()
+{
 	int input = 0;
 	do
 	{
 		menu();
-		printf("请选择：");
 		scanf("%d", &input);
 		switch (input)
 		{
-			case 1:
-				game();
-					break;
-			case 0:
-				printf("退出\n");
-					break;
-			default:
-				printf("选择错误，请重新选择\n");
-				break;
+		case 0:
+			printf("退出游戏！\n");
+			break;
+		case 1:
+			game();
+			break;
+			default：
+				printf("输入错误，请重新输入！\n");
+			break;
 		}
-	
 	} while (input);
-	system("pause");
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	test();
 	return 0;
-
 }
