184a39f586453e6f6f839a633eba5f8f5f83c9af
kelandisi <1191054822@qq.com>
2019-06-03 23:18:05 +0800

    提交代码

---
+++ C_Training camp/Subject1000—1006/Subject1——/test.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	//int a = 0;
+	//int b = 0;
+	//while (scanf("%d%d", &a, &b) != EOF)
+	//{
+	//	printf("%d\n", a + b);
+	//}
+
+
+
+
+
+	/*printf("**************************\n");
+	printf("Hello World!\n");
+	printf("**************************\n");*/
+
+
+
+
+
+	//int arr[3] = { 0 };
+	//int i = 0;
+	//int max = 0;
+	//for (i = 0; i < 3; i++)
+	//{
+	//	scanf("%d", &arr[i]);
+	//}
+	//for (i = 1; i < 3; i++)
+	//{
+	//	max = arr[0];
+	//	if (arr[i] > max)
+	//		max = arr[i];
+	//}
+	//printf("%d\n", max);
+
+
+
+
+
+	/*int c1 = 'C';
+	int c2 = 'h';
+	int c3 = 'i';
+	int c4 = 'n';
+	int c5 = 'a';
+	printf("%c%c%c%c%c", c1 + 4, c2 + 4, c3 + 4, c4 + 4, c5 + 4);*/
+
+
+
+
+
+	/*int i = 0;
+	int n = 0;
+	int arr[50];
+	while (scanf("%d", &n),n!=0)
+	{
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 3;
+		for (i = 4; i <= n; i++)
+		{
+			arr[i] = arr[i - 1] + arr[i - 3];
+		}
+		printf("%d\n", arr[n]);
+	}*/
+
+
+
+
+
+	//double f = 0;
+	//double c = 0;
+	//scanf("%lf", &f);
+	//c = 5 * (f - 32) / 9;
+	//printf("c=%0.2lf", c);
+	system("pause");
+	return 0;
+}
