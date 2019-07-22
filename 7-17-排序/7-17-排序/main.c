b9f4b17adac7ec410df175955b516f1f1d4097f2
kelandisi <1191054822@qq.com>
2019-07-22 15:55:28 +0800

    更新代码（归并和计数）

--- 7-17-排序/7-17-排序/main.c
+++ 7-17-排序/7-17-排序/main.c
@@ -17,10 +17,10 @@ Swap(int *a, int *b)
 	*b = tmp;
 }
 /*
-冒泡排序：
-	(1).时间复杂度：  最好O(n)：      最坏：O(n^2)      平均：O(n^2)
-	(2).空间复杂度：  O(1)
-	(3).稳定性：稳定
+	冒泡排序：
+		(1).时间复杂度：  最好O(n)：      最坏：O(n^2)      平均：O(n^2)
+		(2).空间复杂度：  O(1)
+		(3).稳定性：稳定
 */
 void BubbleSort(int array[],int size)
 {
@@ -94,7 +94,7 @@ void ShellSort(int *a, int size)
 	}
 }
 /*
-	选择排序：
+	直接选择排序：
 		(1).时间复杂度：  O(n)
 		(2).空间复杂度：  O(1)
 		(3).稳定性：不稳定
@@ -166,8 +166,8 @@ void HeapSort(int *a, int size)
 }
 /*
 	快速排序：
-		(1).时间复杂度：
-		(2).空间复杂度：
+		(1).时间复杂度：	O（n*logn）
+		(2).空间复杂度：	O（n）-O（n*logn）
 		(3).稳定性：不稳定
 */
 #if 0
@@ -225,17 +225,92 @@ void QuickSort(int *a, int size)
 {
 	_QuickSort(a, 0, size - 1);
 }
-
+/*
+	归并排序（分治）：
+		(1).时间复杂度：	O（n*logn）
+		(2).空间复杂度：	O（n）
+		(3).稳定性：稳定
+*/
+void Merge(int* a, int begin1, int end1, int begin2, int end2, int* tmp)
+{
+	int index = begin1;
+	int i = begin1;
+	int j = begin2;
+	while (i<=end1 && j<=end2)
+	{
+		if (a[i] <= a[j])
+			tmp[index++] = a[i++];
+		else
+			tmp[index++] = a[j++];
+	}
+	while (i <= end1)
+	{
+		tmp[index++] = a[i++];
+	}
+	while (j <= end2)
+	{
+		tmp[index++] = a[j++];
+	}
+	memcpy(a + begin1, tmp + begin1, sizeof(int)*(end2 - begin1 + 1));
+}
+void Mergesort(int* a,int left, int right,int* tmp)
+{
+	if (left >= right)
+		return;
+	int mid = left + ((right - left) >> 1);
+	Mergesort(a, left, mid, tmp);
+	Mergesort(a, mid+1, right, tmp);
+	Merge(a, left, mid, mid + 1, right, tmp);
+}
+/*
+	计数排序（分治）：
+		(1).时间复杂度：	Ο(n+k)（其中k是整数的范围）
+		(2).空间复杂度：	O（n）
+		(3).稳定性：稳定
+*/
+void CountSort(int* a, int size)
+{
+	int max = a[0];
+	int min = a[0];
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (a[i] > max)
+			max = a[i];
+		if(a[i]<min)
+			min = a[i];
+	}
+	int range = max - min + 1;
+	int *tmp = (int*)calloc(range, sizeof(int));
+	for (i = 0; i < size; i++)
+	{
+		tmp[a[i] - min] += 1;
+	}
+	for (int i = 0; i < range; i++)
+	{
+		while (tmp[i]--) 
+		{
+			a[j++] = i + min;
+		}
+	}
+	free(tmp);
+	tmp = NULL;
+}
 int main()
 {
 	int array[] = { 11,8,9,1,4,6,3,7,2,5 };
-	int size = sizeof(array) / sizeof(array[0]);
+	int size = sizeof(array) / sizeof(int);
+	int* tmp = (int*)calloc(size,sizeof(int)*(sizeof(array) / sizeof(int)));
+	//memset(tmp, -1, sizeof(array) / sizeof(int));
 	//BubbleSort(array,size);
 	//InsertSort(array, size);
 	//ShellSort(array, size);
 	//SelectSort(array, size);
 	//HeapSort(array, size);
-	QuickSort(array,size);
+	//QuickSort(array,size);
+	//Mergesort(array, 0, size - 1, tmp);
+	CountSort(array, size);
 	Print(array,size);
 	system("pause");
 	return 0;
