673203bf3320668232f29899f194b198f084b7aa
kelandisi <1191054822@qq.com>
2019-12-16 22:20:07 +0800

    提交代码

---
+++ 算法题/判断其是一个合法的括号串/判断其是一个合法的括号串/test.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+class Parenthesis {
+
+public:
+
+	bool chkParenthesis(string A, int n) {
+
+		// write code here
+
+		int l = 0; //×óÀ¨ºÅÊý
+
+		for (int i = 0; i < A.length(); i++)
+
+		{
+
+			char c = A[i];
+
+			if (c == '(') {
+
+				l++;
+
+			}
+			else if (c == ')') {
+
+				if (l > 0) {
+
+					l--;
+
+				}
+				else {
+
+					return false;
+
+				}
+
+			}
+			else {
+
+				return false;
+
+			}
+
+		}
+
+		return l == 0;
+
+	}
+
+};
