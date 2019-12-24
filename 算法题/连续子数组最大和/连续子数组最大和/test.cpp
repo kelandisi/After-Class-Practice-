184b2262c0ff673f9c2c2a3034dbdad8be296395
kelandisi <1191054822@qq.com>
2019-12-24 16:11:56 +0800

    更新代码（加入动态规划）

--- 算法题/连续子数组最大和/连续子数组最大和/test.cpp
+++ 算法题/连续子数组最大和/连续子数组最大和/test.cpp
@@ -1,31 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
-int main()		//时间复杂度为0（n^2）
+//暴力法 复杂度过高（o（n^2）)
+int maxSubArray(vector<int> nums)
 {
-	int n = 0;
-	cin >> n;
-	vector<int> arr(n);
-	for (int i = 0; i < n; i++)
+	int max = INT_MIN;
+	for (int i = 0; i < nums.size(); ++i)
 	{
-		cin >> arr[i];
-	}
-		int SUM = -100000;   //给定一个足够小的最大值
-		for (int i = 0; i < arr.size(); i++)
+		int sum = 0;
+		for (int j = i; j < nums.size(); ++j)
 		{
-			int subOfArr = 0;  //临时最大值
-			for (int j = i; j < arr.size(); j++)
-			{
-				subOfArr += arr[j];
-
-				if (subOfArr > SUM)
-				{
-					SUM = subOfArr;
-				}
-			}
+			sum += nums[j];
+			if (sum > max)
+				max = sum;
 		}
-		cout<< SUM;	
+	}
+	return max;
+}
+
+
+//动态规划
+int maxSubArraydp(vector<int> nums)
+{
+	int result = INT_MIN;
+	vector<int> dp(nums.size());
+	dp[0] = nums[0];
+	result = dp[0];
+	for (int i = 1; i < nums.size(); ++i)
+	{
+		dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+		result = max(result, dp[i]);
+	}
+	return result;
+}
+int main()		
+{
+	vector<int> nums;
+	int n = 0;
+	while (cin>>n)
+	{
+		nums.push_back(n);
+		if (cin.get() == '\n')
+		{
+			break;
+		}
+	}
+
+	int ret = maxSubArraydp(nums);
+	cout << ret;
 	system("pause");
 	return 0;
 }
