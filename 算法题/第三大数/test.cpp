a07fcac80adfb4ee474033cdb39e4f951fff3d6a
kelandisi <1191054822@qq.com>
2020-01-18 11:36:08 +0800

    提交代码

---
+++ 算法题/第三大数/test.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<set>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+	int thirdMax(vector<int>& nums)
+	{
+		set<int> s(nums.begin(), nums.end());
+		auto it = s.rbegin();
+		if (s.size() < 3)
+			return *it;
+		else
+			return *(++(++it));
+	}
+};
+int main()
+{
+	Solution s;
+	vector <int> arr;
+	int a = 0;
+	int ret = 0;
+	while (cin >> a)
+	{
+		arr.push_back(a);
+		if (cin.get() == ('\n'))
+			break;
+	}
+	ret = s.thirdMax(arr);
+	cout << ret;
+	system("pause");
+
+	return 0;
+}
