6639b260ea895e376c105f6d1cd1a85b8f5addda
kelandisi <1191054822@qq.com>
2020-01-19 23:02:47 +0800

    提交代码

---
+++ 算法题/跳台阶/test.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+	int climbStairs(int n) {
+		if (n == 1) return 1;
+		int a = 1;
+		int b = 2;
+		for (int i = 3; i <= n; i++)
+		{
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+		return b;
+	}
+};
+int main()
+{
+	Solution s;
+	int n = 0;
+	cin >> n;
+	int ret = s.climbStairs(n);
+	cout << ret;
+	system("pause");
+	return 0;
+}
