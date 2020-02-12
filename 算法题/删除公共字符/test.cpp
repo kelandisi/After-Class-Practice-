45558c6ed967d749d967d95af22c16a14402e936
kelandisi <1191054822@qq.com>
2020-02-12 19:33:50 +0800

    提交代码

---
+++ 算法题/删除公共字符/test.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string s1, s2;
+	getline(cin, s1);
+	getline(cin, s2);
+	for (int i = 0; i < s2.size(); i++)
+	{
+		int index;
+		while ((index = s1.find(s2[i]))!=- 1)
+		{
+			s1.erase(index, 1);
+		}
+	}
+	cout << s1 << endl;
+	return 0;
+}
