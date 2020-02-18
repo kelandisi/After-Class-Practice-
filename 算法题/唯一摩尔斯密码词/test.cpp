32dba7c60b01fa58d79cfa92c49688453be5d0a0
kelandisi <1191054822@qq.com>
2020-02-18 22:33:13 +0800

    提交代码（stl解法）

---
+++ 算法题/唯一摩尔斯密码词/test.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+class Solution {
+public:
+	int uniqueMorseRepresentations(vector<string>& words) {
+		vector<string> codes{ ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+		unordered_set<string> s;
+		for (auto& word : words) {
+			string e;
+			for (auto c : word) {
+				e += codes[c - 'a'];
+			}
+			s.insert(e);
+		}
+		return s.size();
+	}
+};
+int main()
+{
+	string str;
+	vector <string> arr;
+	while (cin>>str)
+	{
+		arr.push_back(str);
+		if (cin.get() == '\n')
+			break;
+	}
+	Solution s;
+	int ret = s.uniqueMorseRepresentations(arr);
+	cout << ret;
+	system("pause");
+	return 0;
+}
