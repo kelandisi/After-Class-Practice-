dc53157f9a227471d63e554844343fd505085799
kelandisi <1191054822@qq.com>
2020-03-21 23:20:24 +0800

    提交代码

---
+++ 算法题/判定是否互为字符重排/test.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+	bool CheckPermutation(string s1, string s2) {
+		if (s1.size() != s2.size()) {
+			return false;
+		}
+		int res = 0;
+		for (int i = 0; i < s1.size(); i++) {
+			res = res ^ s1[i] ^ s2[i];
+		}
+		if (res != 0) {
+			return false;
+		}
+		return true;
+	}
+};
+int main()
+{
+	Solution s;
+	string str1;
+	string str2;
+	cin >> str1 >> str2;
+	if (s.CheckPermutation(str1, str2))
+	{
+		cout << "ture";
+	}
+	else
+		cout << "false";
+	system("pause");
+	return 0;
+}
+
