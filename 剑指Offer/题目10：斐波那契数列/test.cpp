3746a202565bab0aff7392e22308e4d13b879726
kelandisi <1191054822@qq.com>
2020-06-02 22:37:13 +0800

    提交代码

---
+++ 剑指Offer/题目10：斐波那契数列/test.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+int Fibo(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	if (n == 2)
+		return 1;
+	int one = 1;
+	int two = 1;
+	int sum = 0;
+	for (int i = 3; i <= n; i++)
+	{
+		sum = one + two;
+		one = two;
+		two = sum;
+	}
+	return sum;
+}
+
+//青蛙跳台阶问题
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	cout<<Fibo(n);
+	system("pause");
+	return 0;
+}
