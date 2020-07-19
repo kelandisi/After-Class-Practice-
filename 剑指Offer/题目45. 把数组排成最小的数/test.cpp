c78415e72026a0ac5acf3271cf192ab52ed30837
kelandisi <1191054822@qq.com>
2020-07-19 23:01:14 +0800

    提交代码

---
+++ 剑指Offer/题目45. 把数组排成最小的数/test.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+	string minNumber(vector<int>& nums) {
+		vector<string>strs;
+		string ans;
+		for (int i = 0; i < nums.size(); i++) {
+			strs.push_back(to_string(nums[i]));
+		}
+		sort(strs.begin(), strs.end(), [](string& s1, string& s2) {return s1 + s2 < s2 + s1; });
+		for (int i = 0; i < strs.size(); i++)
+			ans += strs[i];
+		return ans;
+	}
+};
+
