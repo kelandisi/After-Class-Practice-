77735d4a843f26d6a16464b940725eca37a447bd
kelandisi <1191054822@qq.com>
2020-07-23 23:31:58 +0800

    提交代码

---
+++ 剑指Offer/题目52. 两个链表的第一个公共节点/test.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *node1 = headA;
+		ListNode *node2 = headB;
+
+		while (node1 != node2) {
+			node1 = node1 != NULL ? node1->next : headB;
+			node2 = node2 != NULL ? node2->next : headA;
+		}
+		return node1;
+	}
+};
+
