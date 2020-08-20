aeac6014b56769b25a003420224bb280176dad69
kelandisi <1191054822@qq.com>
2020-08-20 23:47:31 +0800

    提交代码

---
+++ 剑指Offer/题目68 - I. 二叉搜索树的最近公共祖先/test.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if ((root->val - p->val)*(root->val - q->val) <= 0) {
+			return root;
+		}
+		if (root->val > p->val) {
+			return lowestCommonAncestor(root->left, p, q);
+		}
+		if (root->val < p->val) {
+			return lowestCommonAncestor(root->right, p, q);
+		}
+		return NULL;
+	}
+};
