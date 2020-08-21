1064d36a468aefea7bda0cf8e77f2d659e054058
kelandisi <1191054822@qq.com>
2020-08-21 23:31:31 +0800

    提交代码

---
+++ 剑指Offer/题目68 - II. 二叉树的最近公共祖先/test.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == NULL)return NULL;
+		if (p->val > root->val && q->val > root->val)
+			return lowestCommonAncestor(root->right, p, q);
+		else if (p->val < root->val && q->val < root->val)
+			return lowestCommonAncestor(root->left, p, q);
+		else return root;
+	}
+};
+
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == NULL)return NULL;
+		if (root == p || root == q)return root;
+		TreeNode* left = lowestCommonAncestor(root->left, p, q);
+		TreeNode* right = lowestCommonAncestor(root->right, p, q);
+		if (left && right)return root;
+		return left ? left : right; // 只有一个非空则返回该指针，两个都为空则返回空指针
+	}
+};
+
